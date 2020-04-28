7423c0d91832e69b7f44ee6e42b2ffdeb7794451
Chris Giroux <cgiroux86@gmail.com>
2020-04-28 11:43:22 -0400

    another attempt at psql put fix

--- utils/helpers.js
+++ utils/helpers.js
@@ -68,9 +68,7 @@ module.exports = {
   },
 
   editSleep1(id, info) {
-    return db.raw(
-      `select users_sleep set sleep_start=${info.sleep_start}, start_score=${info.start_score}, sleep_end=${info.sleep_end}, end_score=${info.end_score}, overall_score=${info.overall_score} where id=${id}`
-    );
+    return db("users_sleep").where({ id }).update(info);
   },
 
   //deletes a sleep_session, requires user id and sleep_session id
